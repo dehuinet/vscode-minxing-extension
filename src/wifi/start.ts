629a254ecb094ef0a4a68d617cc707076f5d2a69
stuart_zhang <stuartpage@qq.com>
2018-01-10 18:21:18 +0800

    Use the class to re-implement the StatusBarItem.

--- src/wifi/start.ts
+++ src/wifi/start.ts
@@ -3,14 +3,34 @@ import * as vscode from 'vscode';
 import * as _ from 'underscore';
 import * as Utils from '../utils';
 import {WifiInfo} from '../domain';
-let statusBarItem;
-function setStatusBarMessage(clientIps:Array<string> = []) {
-    const {port, ip, connectionCount} : WifiInfo = MXAPI.Wifi.info() as WifiInfo;
-    const ips = clientIps.map(ip => ip.replace(/^::ffff:/i, ''));
-    const ipStr = _.isEmpty(ips) ? '' : `,客户端:${ips.join(', ')}`;
-    const status = `IP:${ip.join(' | ')}, 端口:${port},连接数:${connectionCount}${ipStr}`;
-    statusBarItem.text = status;
-    return statusBarItem;
+class StatusBarItem{
+    static _instance: StatusBarItem;
+    static get instance(){
+        if (StatusBarItem._instance == null) {
+            StatusBarItem._instance = new StatusBarItem();
+        }
+        return StatusBarItem._instance;
+    }
+    ctrl: vscode.StatusBarItem;
+    constructor(){
+        this.ctrl = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Left, 2);
+        _.extendOwn(this.ctrl, {
+            tooltip: '客户端连接状态',
+            command: 'Minxing.getWifiInfo'
+        });
+        this.setMsg();
+        this.ctrl.show();
+    }
+    setMsg(clientIps:Array<string> = []){
+        const {port, ip, connectionCount} : WifiInfo = MXAPI.Wifi.info() as WifiInfo;
+        const ips = clientIps.map(ip => ip.replace(/^::ffff:/i, ''));
+        const ipStr = _.isEmpty(ips) ? '' : `,客户端:${ips.join(', ')}`;
+        const status = `IP:${ip.join(' | ')}, 端口:${port},连接数:${connectionCount}${ipStr}`;
+        this.ctrl.text = status;
+    }
+    dispose(){
+        this.ctrl.dispose();
+    }
 }
 export default {
     start(context) {
@@ -19,22 +39,17 @@ export default {
         MXAPI.Wifi.start({
             tempPath, port,
             onConnection(clientIps:Array<string>, clientIp:string){
-                setStatusBarMessage(clientIps);
+                StatusBarItem.instance.setMsg(clientIps);
                 vscode.window.showInformationMessage(`调试终端 [${clientIp.replace(/^::ffff:/i, '')}] 已连接到 VSCode。可以开始调试了...`);
             },
             onClose(clientIps:Array<string>, clientIp:string){
-                setStatusBarMessage(clientIps);
+                StatusBarItem.instance.setMsg(clientIps);
                 vscode.window.showInformationMessage(`调试终端 [${clientIp.replace(/^::ffff:/i, '')}] 已断离 VSCode`);
             }
         });
-        statusBarItem = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Left, 2);
-        statusBarItem.tooltip = '客户端连接状态';
-        statusBarItem.command = 'Minxing.getWifiInfo';
-        context.subscriptions.push(statusBarItem);
-        setStatusBarMessage();
-        statusBarItem.show();
+        context.subscriptions.push(StatusBarItem.instance.ctrl);
     },
     stop(){
-        statusBarItem && statusBarItem.dispose();
+        StatusBarItem.instance.dispose();
     }
 };

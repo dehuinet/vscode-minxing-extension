0244e1eacfc07234bf169b6f4f4408dd7c672f85
stuart_zhang <stuartpage@qq.com>
2018-01-10 17:03:37 +0800

    Split the wifi.ts file into multiple smaller files.

--- src/wifi/index.ts
+++ src/wifi/index.ts
@@ -3,28 +3,13 @@ import * as path from 'path';
 import * as vscode from 'vscode';
 import * as MXAPI from 'minxing-devtools-core';
 import co from 'co';
-import {WifiInfo, LocalStorage} from './domain';
-import * as Utils from './utils';
-import output from './output';
+import {WifiInfo, LocalStorage} from '../domain';
+import * as Utils from '../utils';
+import output from '../output';
+import start from './start';
 
 export default {
-    start() {
-        const tempPath: string = Utils.getTempPath();
-        const port: number =  Utils.getRandomNum(1001, 9999);
-        const that = this;
-        MXAPI.Wifi.start({
-            tempPath, port,
-            onConnection(clientIps:Array<string>, clientIp:string){
-                that.setStatusBarMessage(clientIps);
-                vscode.window.showInformationMessage(`调试终端 [${clientIp.replace(/^::ffff:/i, '')}] 已连接到 VSCode。可以开始调试了...`);
-            },
-            onClose(clientIps:Array<string>, clientIp:string){
-                that.setStatusBarMessage(clientIps);
-                vscode.window.showInformationMessage(`调试终端 [${clientIp.replace(/^::ffff:/i, '')}] 已断离 VSCode`);
-            }
-        });
-        this.setStatusBarMessage();
-    },
+    ...start,
     clearTempCache() {
         const tempPath: string = Utils.getTempPath();
         MXAPI.clearTemp(tempPath);
@@ -48,17 +33,6 @@ export default {
         .then(() => {
             console.log("启动WiFi日志服务...");
         })
-    },
-    setStatusBarMessage(clientIps:Array<string> = []) {
-        const {
-            port,
-            ip,
-            connectionCount
-        } : WifiInfo = MXAPI.Wifi.info() as WifiInfo;
-        const ips = clientIps.map(ip => ip.replace(/^::ffff:/i, ''));
-        const ipStr = _.isEmpty(ips) ? '' : `,客户端:${ips.join(', ')}`;
-        const status = `IP:${ip.join(' | ')}, 端口:${port},连接数:${connectionCount}${ipStr}`;
-        vscode.window.setStatusBarMessage(status);
     },
     getWifiInfo() {
         const {
---
+++ src/wifi/start.ts
@@ -0,0 +1,30 @@
+import * as MXAPI from 'minxing-devtools-core';
+import * as vscode from 'vscode';
+import * as _ from 'underscore';
+import * as Utils from '../utils';
+import {WifiInfo} from '../domain';
+function setStatusBarMessage(clientIps:Array<string> = []) {
+    const {port, ip, connectionCount} : WifiInfo = MXAPI.Wifi.info() as WifiInfo;
+    const ips = clientIps.map(ip => ip.replace(/^::ffff:/i, ''));
+    const ipStr = _.isEmpty(ips) ? '' : `,客户端:${ips.join(', ')}`;
+    const status = `IP:${ip.join(' | ')}, 端口:${port},连接数:${connectionCount}${ipStr}`;
+    vscode.window.setStatusBarMessage(status);
+}
+export default {
+    start() {
+        const tempPath: string = Utils.getTempPath();
+        const port: number =  Utils.getRandomNum(1001, 9999);
+        MXAPI.Wifi.start({
+            tempPath, port,
+            onConnection(clientIps:Array<string>, clientIp:string){
+                setStatusBarMessage(clientIps);
+                vscode.window.showInformationMessage(`调试终端 [${clientIp.replace(/^::ffff:/i, '')}] 已连接到 VSCode。可以开始调试了...`);
+            },
+            onClose(clientIps:Array<string>, clientIp:string){
+                setStatusBarMessage(clientIps);
+                vscode.window.showInformationMessage(`调试终端 [${clientIp.replace(/^::ffff:/i, '')}] 已断离 VSCode`);
+            }
+        });
+        setStatusBarMessage();
+    }
+};
